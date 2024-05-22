87c21262228f394cb90924fdde89604def94fba9
Vedant Rajendra Balpande <136176599+vedant-35@users.noreply.github.com>
2024-05-22 22:32:58 +0530

    designing changes are made

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-const express = require('express');
+const express = require("express");
 const articleRouter = require("./routes/articles");
 //const Article = require('./models/article');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //const methodOverride = require('method-override');
 const app = express();
 
-mongoose.connect('mongodb://localhost/bharatInternDatabase');
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index');
-})
+mongoose.connect("mongodb://localhost/bharatInternDatabase");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      titles: "Test Article 1",
+      createdAt: new Date(),
+      description: "Test Description",
+    },
+    {
+      titles: "Test Article 2",
+      createdAt: new Date(),
+      description: "Test Description",
+    },
+  ];
+  res.render("articles/index", { articles: articles });
+});
 
-app.use('/articles', articleRouter);
+app.use("/articles", articleRouter);
 
-app.listen(3000)
+app.listen(3000);
