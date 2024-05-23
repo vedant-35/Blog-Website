cb252efab8cfc4886c17606abf1198e1fdf24f8b
Vedant Rajendra Balpande <136176599+vedant-35@users.noreply.github.com>
2024-05-23 15:07:48 +0530

    chore: Update article routes and form fields

--- routes/articles.js
+++ routes/articles.js
@@ -3,14 +3,15 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('articles/new');
+    res.render('articles/new', { article: new Article() });
 })
-router.put(`/:id`,(req,res)=>{
-
-
+router.get(`/:id`,async(req,res)=>{
+    const article = await Article.findById(req.params.id)
+    if(article==null) res.redirect('/')
+    res.render('articles/edit',{article:article})
 })
 router.post('/', async(req, res) => {
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown
